000f4d8e938e5c2f08f4ea5bf33dc818773a9710
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-06-20 07:21:39 -0400

    Finish draft of password checker.

--- checkmypass.py
+++ checkmypass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 # password123 -> CBFDAC6008F9CAB4083784CBD1874F76618D2A97
 
@@ -15,9 +16,35 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':')
+              for line in hashes.text.splitlines())  # generator object
+
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     # check password if it exists in API response
-    return hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+  # same as above
 
 
-pwned_api_check('password123')  # same as above
+def main(password_list):
+    for password in password_list:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times... you should probably change your password')
+        else:
+            print(f'{password} was NOT found.  Carry on!')
+    return 'done'
+
+
+main(sys.argv[1:])
